3d938498ad529229b698ac39121a52af8d8e1a37
mohamed <mohamedhamdy994@gmail.com>
2024-05-30 20:15:11 -0700

    classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,7 @@
+// Define the ClassRoom class
+export default class ClassRoom {
+  // Constructor to initialize maxStudentsSize
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
